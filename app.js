3c84f1221cae04ff5fca607cc9f6cb09586e8594
Sidduri2025 <vaishusidduri@gmail.com>
2025-03-25 20:00:30 -0500

    Added files

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/', mydataRouter);
+app.use('/mydata', mydataRouter);
+app.get('/mydata', function (req, res) {
+  res.send('This is the /mydata route!');
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
